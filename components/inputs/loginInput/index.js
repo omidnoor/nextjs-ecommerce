211e21c076222e90eccda31a67711fbe8d64d76b
Omid Noorshams <o.noorshams@gmail.com>
2023-04-04 19:31:01 -0700

    create login email and password input and validatian with Formik and Yup

--- components/inputs/loginInput/index.js
+++ components/inputs/loginInput/index.js
@@ -1,12 +1,20 @@
+import { useField } from "formik";
+
 import { BiUser } from "react-icons/bi";
 import { SiMinutemailer } from "react-icons/si";
 import { IoKeyOutline } from "react-icons/io5";
 
 import styles from "./styles.module.scss";
 
-export default function LoginInput({ icon, placeholder }) {
+export default function LoginInput({ icon, placeholder, ...props }) {
+  const [field, meta] = useField(props);
+
   return (
-    <div className={styles.input}>
+    <div
+      className={`${styles.input} ${
+        meta.touched && meta.error ? styles.error : ""
+      }`}
+    >
       {icon === "user" ? (
         <BiUser />
       ) : icon === "email" ? (
@@ -16,7 +24,13 @@ export default function LoginInput({ icon, placeholder }) {
       ) : (
         ""
       )}
-      <input type="text" placeholder={placeholder} />
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
     </div>
   );
 }
--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { Form, Formik } from "formik";
+import * as Yup from "yup";
 import Link from "next/link";
 
 import { BiLeftArrowAlt } from "react-icons/bi";
@@ -8,7 +10,27 @@ import { BiLeftArrowAlt } from "react-icons/bi";
 import styles from "../styles/signin.module.scss";
 import LoginInput from "@/components/inputs/loginInput";
 
+const initialValues = {
+  login_email: "",
+  login_password: "",
+};
+
 export default function signin() {
+  const [user, setUser] = useState(initialValues);
+  const { login_email, login_password } = user;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email"),
+    login_password: Yup.string().required("Password is required"),
+  });
+
   return (
     <>
       <Header country="Canada" />
@@ -26,10 +48,31 @@ export default function signin() {
           <div className={styles.login__form}>
             <h1>Sign in</h1>
             <p>Get access to one of teh best Eshopping services.</p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                login_email,
+                login_password,
+              }}
+              validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form>
-                  <LoginInput icon="password" placeholder="password" />
+                  <LoginInput
+                    type="email"
+                    name="login_email"
+                    icon="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                  />
+
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
                 </Form>
               )}
             </Formik>
