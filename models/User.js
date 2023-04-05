c2c4170c40e166d7f000c73b44a4758c860eab82
Omid Noorshams <o.noorshams@gmail.com>
2023-04-05 15:56:36 -0700

    create user schema with mongoose in models folder

---
+++ models/User.js
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.schema(
+  {
+    name: {
+      type: String,
+      required: "Please enter your full name",
+    },
+    email: {
+      type: String,
+      required: "Please enter your email",
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: "Please enter your password",
+      minlength: 8,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    imgae: {
+      type: String,
+      default: "@/images/default/default-user.png",
+    },
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    defaultPaymentMethod: {
+      type: String,
+      default: "",
+    },
+    address: [
+      {
+        firstName: {
+          type: String,
+        },
+        lastName: {
+          type: String,
+        },
+        phoneNumber: {
+          type: String,
+        },
+        address1: {
+          type: String,
+        },
+        address2: {
+          type: String,
+        },
+        city: {
+          type: String,
+        },
+        zipCode: {
+          type: String,
+        },
+        state: {
+          type: String,
+        },
+        country: {
+          type: String,
+        },
+        active: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
