f84196043cd6070ea0bd8e0fb0a6b9f4c6f1d83a
Omid Noorshams <o.noorshams@gmail.com>
2023-04-07 19:07:01 -0700

    auth-test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,71 +1,20 @@
 import NextAuth from "next-auth";
-import bcrypt from "bcrypt";
-
+import AppleProvider from "next-auth/providers/apple";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
-import Auth0Provider from "next-auth/providers/auth0";
-import CredentialsProvider from "next-auth/providers/credentials";
-
+// import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-
-import db from "@/utils/db";
 import clientPromise from "./lib/mongodb";
-import User from "@/models/User";
-
-db.connectDb();
-
-const SignInUser = async ({ password, user }) => {
-  if (!password) {
-    throw new Error("Password is required");
-  }
-  const testPassword = await bcrypt.compare(password, user.password);
-
-  if (!testPassword) {
-    throw new Error("Email or password is incorrect");
-  }
-
-  return user;
-};
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
 
   providers: [
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const email = credentials.email;
-        const password = credentials.password;
-
-        const user = await User.findOne({ email });
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-
-          return await SignInUser({ password, user });
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          throw new Error("Incorrect email.");
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
+    // OAuth authentication providers...
+    AppleProvider({
+      clientId: process.env.APPLE_ID,
+      clientSecret: process.env.APPLE_SECRET,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
@@ -79,17 +28,12 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    // Passwordless / email sign in
+    // EmailProvider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: "NextAuth.js <no-reply@example.com>",
+    // }),
   ],
-
-  callbacks: {
-    async session({ session, token }) {
-      let user = await User.findById(token.sub);
-      session.user.id = token?.sub || user?._id.toString();
-      session.user.role = user?.role || "user";
-
-      return session;
-    },
-  },
 
   pages: {
     signIn: "/signin",
