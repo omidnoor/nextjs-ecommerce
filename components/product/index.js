244cbe10a15a94d262d8eb1039900b12405b16d4
Omid Noorshams <o.noorshams@gmail.com>
2023-04-11 17:14:33 -0700

    use react-image-magnify in product single page component. responsiveness of product images

--- components/product/index.js
+++ components/product/index.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Head from "next/head";
 
 import styles from "./styles.module.scss";
 import Header from "../header";
 import Footer from "../footer";
+import MainSwiper from "./MainSwiper";
 
 export default function ProductSingle({ product, country }) {
+  const [activeImg, setActiveImg] = useState("");
   console.log(product);
   return (
     <>
@@ -14,11 +17,14 @@ export default function ProductSingle({ product, country }) {
       <Header country={country} />
       <div className={styles.product}>
         <div className={styles.container}>
-          <div className={styles.path}>
+          <div className={styles.product__path}>
             Home / {product.category.name}{" "}
             {product.subCategories.map((sub, index) => (
               <span> / {sub.name}</span>
             ))}
+          </div>
+          <div className={styles.product__main}>
+            <MainSwiper images={product.images} activeImg={activeImg} />
           </div>
         </div>
       </div>
