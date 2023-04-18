d9bb0cc68d5067b465378af02a335b544d4c2cba
Omid Noorshams <o.noorshams@gmail.com>
2023-04-17 17:33:00 -0700

    modify scss to rearrange cart sections and responsiveness. add checkout component

--- components/cart/cartContainer/checkout/index.js
+++ components/cart/cartContainer/checkout/index.js
@@ -1,5 +1,24 @@
 import styles from "./styles.module.scss";
 
 export default function Checkout({ subtotal, shippingFee, total, selected }) {
-  return <div className={styles.cart__checkout}>checkout</div>;
+  return (
+    <div className={styles.checkout}>
+      <h2>Order Summary</h2>
+      <div className={styles.checkout_line}>
+        <span>Subtotal</span>
+        <span>CAD{subtotal}$</span>
+      </div>
+      <div className={styles.checkout_line}>
+        <span>Shipping</span>
+        <span>+{shippingFee}$</span>
+      </div>
+      <div className={styles.checkout_total}>
+        <span>Total</span>
+        <span>CAD{total}$</span>
+      </div>
+      <div className={styles.submit}>
+        <button>Continue</button>
+      </div>
+    </div>
+  );
 }
--- components/cart/cartContainer/index.js
+++ components/cart/cartContainer/index.js
@@ -10,16 +10,18 @@ export default function CartContainer({ cart }) {
     <div className={styles.cart}>
       {cart.items.length > 0 ? (
         <div className={styles.cart__container}>
+          {/* <div className={styles.cart_container_inner}> */}
           <CartHeaderContainer items={cart.items} />
           <div className={styles.cart__products}>
             {cart.items.map((item, index) => (
               <CartItem key={item._uid} item={item} />
             ))}
           </div>
-          <div className={styles.card}>
+          {/* </div> */}
+          <div className={`${styles.cart__checkout} ${styles.card}`}>
             <Checkout
               subtotal="5454"
-              shippingFee=""
+              shippingFee={0}
               total="4554"
               selected={[]}
             />
