c2c067a33bb31bf9c116a6bd2eb5d2a3ae4213e1
Omid Noorshams <o.noorshams@gmail.com>
2023-04-21 16:29:28 -0700

    create order page. create scss for order page

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,3 +1,5 @@
+import { IoIosArrowForward } from "react-icons/io";
+
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Order from "@/models/Order";
@@ -9,6 +11,73 @@ export default function OrderPage({ order }) {
   return (
     <div>
       <Header country="Canada" />
+      <div className={styles.order}>
+        <div className={styles.container}>
+          <div className={styles.order__info}>
+            <div className={styles.order__header}>
+              <div className={styles.order__header_head}>
+                Home <IoIosArrowForward /> Orders <IoIosArrowForward /> ID{" "}
+                {order._id}
+              </div>
+              <div className={styles.order__header_status}>
+                Payment Status :{" "}
+                {order.isPaid ? (
+                  <img src="/images/verified.png" alt="verified" />
+                ) : (
+                  <img src="/images/unverified.png" alt="unverified" />
+                )}
+              </div>
+              <div className={styles.order__header_status}>
+                Order Status :{" "}
+                <span
+                  className={
+                    order.status === "Not Processed"
+                      ? styles.not_processed
+                      : order.status === "Processing"
+                      ? styles.processing
+                      : order.status === "Dispatched"
+                      ? styles.dispatched
+                      : order.status === "Cancelled"
+                      ? styles.cancelled
+                      : order.status === "Completed"
+                      ? styles.completed
+                      : ""
+                  }
+                >
+                  {order.status}
+                </span>
+              </div>
+            </div>
+            <div className={styles.order__products}>
+              {order.products.map((product, index) => (
+                <div key={index} className={styles.product}>
+                  <div className={styles.product__img}>
+                    <img src={product.image} alt={product.name} />
+                  </div>
+                  <div className={styles.product__info}>
+                    <h1 className={styles.product__info_name}>
+                      {product.name.length > 30
+                        ? `${product.name.slice(0, 30)}...`
+                        : product.name}
+                    </h1>
+                    <div className={styles.product__info_style}>
+                      <img src={product.color.image} alt="product style" /> /{" "}
+                      {product.size}
+                    </div>
+                    <div className={styles.product__info_price_qty}>
+                      CAD {product.price}$ x {product.qty}
+                    </div>
+                    <div className={styles.product__info_total_price}>
+                      CAD {product.price * product.qty}$
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.order__actions}></div>
+        </div>
+      </div>
 
       {/* <Footer country="Canada" /> */}
     </div>
