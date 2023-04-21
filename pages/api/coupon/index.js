b5172ca966eddd56bf476d887e3e1011b42993aa
Omid Noorshams <o.noorshams@gmail.com>
2023-04-20 20:24:50 -0700

    create coupon api. send test post request with postman

---
+++ pages/api/coupon/index.js
@@ -0,0 +1,58 @@
+import nc from "next-connect";
+import User from "../../../models/User";
+import Coupon from "../../../models/Coupon";
+import db from "../../../utils/db";
+import auth from "../../../middleware/auth";
+const handler = nc();
+
+handler.post(async (req, res) => {
+  try {
+    db.connectDb();
+    const { coupon, startDate, endDate, discount } = req.body;
+    const test = await Coupon.findOne({ coupon });
+    if (test) {
+      return res.status(400).json({
+        message: "This Coupon name already exists, try with a different name.",
+      });
+    }
+
+    await new Coupon({
+      coupon,
+      startDate,
+      endDate,
+      discount,
+    }).save();
+    // console.log(newCoupon);
+
+    db.disconnectDb();
+    return res.json({
+      message: "Coupon created successfully !",
+      coupons: await Coupon.find({}),
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  } finally {
+    db.disconnectDb();
+  }
+});
+
+export default handler;
+
+// import nc from "next-connect";
+// import User from "../../../models/User";
+// import Coupon from "../../../models/Coupon";
+// import db from "../../../utils/db";
+// import auth from "../../../middleware/auth";
+// const handler = nc();
+
+//     db.disconnectDb();
+//     return res.json({
+//       message: "Coupon created successfully !",
+//       coupons: await Coupon.find({}),
+//     });
+//   } catch (error) {
+//     return res.status(500).json({ message: error.message });
+//   }
+// });
+
+// export default handler;
